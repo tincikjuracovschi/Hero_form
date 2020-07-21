fa9b955019a71efb913e4be8b851520eecba573e
unknown <tincik.j@gmail.com>
2020-07-21 11:48:49 +0300

    Init form inputs

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
-$(function() {
+$(".default_option").click(function(){
+	$(this).parent().toggleClass("active");
+	
+  })
+  
+  $(".select_ul li").click(function(){
+	var currentele = $(this).html();
+	$(".default_option li").html(currentele);
+	$(this).parents(".select_wrap").removeClass("active");
+  })
 
-	// Custom JS
 
-});
